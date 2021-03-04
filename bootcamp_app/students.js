c99e0e6bc210e7907c8813168da6462b3e05aa60
Preetirani123 <rani.preeti3333@gmail.com>
2021-03-04 04:18:33 +0000

    SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,3 @@
-const month = process.argv[2];
-const limit = process.argv[3];
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,14 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 
-pool.query(`
+const values = [`%${cohortName}%`, limit];
+
+const queryString =`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students JOIN cohorts
 ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${month}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2`
+;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
   console.log(`${user['name']} has an id of ${user['student_id']} and was in the ${user['cohort']} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const values = [`${cohortName}`];
 
-pool.query(`
+const queryString =`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
   console.log(`${user['cohort']} : ${user['teacher']} `);
